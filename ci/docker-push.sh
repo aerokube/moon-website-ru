748ec45622a9205e2aa1f0ed4cdc21131218ee65
Ivan Krutov <vania-pooh@aerokube.com>
2022-10-24 13:37:22 +0300

    Login to Docker registry

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" registry.aerokube.com
 TAG="registry.aerokube.com/moon-ru/website"
 docker build --pull -t $TAG -t $TAG:$1 .
 docker push $TAG
