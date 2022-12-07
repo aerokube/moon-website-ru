a60d06c80e9749a9031dd53de5236460b779cc2d
Ivan Krutov <vania-pooh@aerokube.com>
2022-12-07 12:15:20 +0300

    Using docker run to build PDF docs

--- ci/docs.sh
+++ ci/docs.sh
@@ -3,6 +3,7 @@
 set -e
 TAG=$1
 WD=$(pwd)
+SOURCE_DIR=${WD}/docs
 OUTPUT_DIR=${WD}/dist/docs
 
 echo "Removing existing files"
@@ -10,4 +11,7 @@ mkdir -p ${OUTPUT_DIR}
 rm -Rf ${OUTPUT_DIR}/*.pdf
 
 echo "Generating docs"
-cd ${WD}/docs && ls *.adoc | xargs -I{} asciidoctor-pdf -a revnumber=${TAG} -D ${OUTPUT_DIR} {}
+set -x
+cd "$SOURCE_DIR" && ls *.adoc | while read file; do
+  docker run --rm --pull always -v "$OUTPUT_DIR:/output" -v "$SOURCE_DIR:/source" asciidoctor/docker-asciidoctor:1 asciidoctor-pdf -a "revnumber=$TAG" -D /output "/source/$file"
+done
